9c6f1f431e74ee828ee0a215728c03e1bad6e253
Sixing Xu <sixingxu@Sixings-iMac.lan>
2025-03-26 16:06:53 -0400

    db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,17 @@
- // path: /config/env/production/database.ts
- export default ({ env }) => ({
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST'), 
-        port: env.int('DATABASE_PORT'), 
-        database: env('DATABASE_NAME'), 
-        user: env('DATABASE_USERNAME'), 
-        password: env('DATABASE_PASSWORD'),
-        ssl: {
-          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-        },
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        ca: env('DATABASE_CA')
       },
-      debug: false,
     },
-  });
+    debug: false,
+  },
+});
