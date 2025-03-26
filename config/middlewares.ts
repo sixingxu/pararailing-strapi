063f8b3a692b502bd66aef03d6f13333e4a9691f
Sixing Xu <sixingxu@Sixings-iMac.lan>
2025-03-26 19:06:53 -0400

    cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,14 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      origin: ['https://pararailing-nextjs-hhmw4.ondigitalocean.app'] // Replace with your actual domain
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
