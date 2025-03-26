3f756508df6762584a7c9f51140e805dbacd8048
Sixing Xu <sixingxu@Sixings-iMac.lan>
2025-03-26 18:32:52 -0400

    env

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://pararailing-strapi-6op58.ondigitalocean.app'),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS')
   },
 });
